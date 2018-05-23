3fc05af11aa47a6de4a88ce4817a9289319e920f
Maxim Pechenyuk <plitochnik@gmail.com>
2018-05-23 15:07:16 +0300

    Clear project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.scss';
+import { InputTest } from './input/Input';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <InputTest/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/input/Input.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export class InputTest extends React.Component<{}> {
+
+  render() {
+    return (
+      <div>
+       1
+      </div>
+    );
+  }
+}
